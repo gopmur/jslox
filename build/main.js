0b2eef132fcc90cae10329ccab8645c3545d441d
Ali Jafari <ali81jafari@outlook.com>
2024-02-19 23:15:26 +0330

    Added jest and pnpm

--- build/main.js
+++ build/main.js
@@ -1,4 +1,9 @@
 "use strict";
-(function main() {
-    console.log("Hello World!");
-})();
+Object.defineProperty(exports, "__esModule", { value: true });
+const commander_1 = require("commander");
+let filePath = "";
+commander_1.program.argument("<input-file>").action((argument) => {
+    filePath = argument;
+});
+commander_1.program.parse(process.argv);
+console.log(filePath);
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,10 @@
-(function main() {
-    console.log("Hello World!");
-})();
+import { program } from "commander";
+
+let filePath = "";
+program.argument("<input-file>").action((argument) => {
+    filePath = argument;
+});
+program.parse(process.argv);
+
+
+console.log(filePath);
